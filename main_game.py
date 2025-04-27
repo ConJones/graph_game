2a6795521104f9c91638d9b92b0effce3ca97fbb
RJasonM <jmeadows2012.jm@gmail.com>
2025-04-27 16:41:56 -0500

    What Jason did today - add random color.

--- main_game.py
+++ main_game.py
@@ -1,14 +1,16 @@
 import pygame
 from pygame.locals import *
 import sys
+import random
 
 # Global Constants
-HEIGHT = 450
-WIDTH = 400
-FPS = 60
+WIDTH = 1920
+HEIGHT = 1080
+FPS = 1
 
 def main():
     # Local Variables
+    color_node = ((119, 185, 224),(99, 101, 207),(186, 125, 219),(255, 115, 157),(255, 238, 135),(166, 207, 99))
 
     # Initialize Variables
     pygame.init()
@@ -23,8 +25,17 @@ def main():
                 pygame.quit()
                 sys.exit()
         
-        displaysurface.fill((0,0,0))
-        
+        displaysurface.fill((51, 51, 51))
+
+        for dot in range(400):       
+            coords = (WIDTH*random.random(),HEIGHT*random.random())
+
+            pygame.draw.line(displaysurface, (255,255,255), coords, (WIDTH*random.random(),HEIGHT*random.random()), 1)
+
+            pygame.draw.circle(displaysurface, random.choice(color_node), coords, 5)
+
+            
+
         pygame.display.update()
         FramePerSec.tick(FPS)
 
