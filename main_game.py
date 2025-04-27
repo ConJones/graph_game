142f70ad3e82fcc29527c9d1bf2d5acbc84e73ff
ConJones <connorj314159@gmail.com>
2025-04-27 15:09:01 -0500

    base window

--- main_game.py
+++ main_game.py
@@ -1,8 +1,32 @@
 import pygame
+from pygame.locals import *
+import sys
+
+# Global Constants
+HEIGHT = 450
+WIDTH = 400
+FPS = 60
 
 def main():
-    # Add your code here
-    print("Hello, world!")
+    # Local Variables
+
+    # Initialize Variables
+    pygame.init()
+    FramePerSec = pygame.time.Clock()
+    displaysurface = pygame.display.set_mode((WIDTH, HEIGHT)) 
+    vec = pygame.math.Vector2 
+
+    # Main loop
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+        
+        displaysurface.fill((0,0,0))
+        
+        pygame.display.update()
+        FramePerSec.tick(FPS)
 
 if __name__ == "__main__":
     main()
