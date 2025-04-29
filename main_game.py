1a4faf4b3cf3d3d0b285fcbf80fc4bdd351bcbe6
RJasonM <jmeadows2012.jm@gmail.com>
2025-04-28 21:51:23 -0500

    Jason Added a Sphere of Influence as well as an alternative way to exit the game.

--- main_game.py
+++ main_game.py
@@ -15,7 +15,10 @@ FPS = 144
 NUM_NODES = 10
 NUM_EDGES = 20
 
-NODE_COLORS = ((119, 185, 224),(99, 101, 207),(186, 125, 219),(255, 115, 157),(255, 238, 135),(166, 207, 99))
+# NODE COLORS in RGBA (Left to right): Blue, Indigo, Purple,
+#                                      Pink, Yellow, Green.
+NODE_COLORS = ((119, 185, 224, 100),(99, 101, 207, 100),(186, 125, 219, 100),
+               (255, 115, 157, 100),(255, 238, 135, 100),(166, 207, 99, 100))
 
 # Classes
 
@@ -40,32 +43,45 @@ def main():
     # Local Variables
     graph = rand_graph()
 
+    # Radius of the circle of influence for player.
+    player_influence_radius = 15
+
     # Initialize Variables
     pygame.init()
     FramePerSec = pygame.time.Clock()
-    displaysurface = pygame.display.set_mode((WIDTH, HEIGHT)) 
+    displaysurface = pygame.display.set_mode((WIDTH, HEIGHT))
+    transparency_surface = pygame.Surface((WIDTH,HEIGHT), pygame.SRCALPHA)
     vec = pygame.math.Vector2 
 
     # Main loop
     while True:
         # Events
         for event in pygame.event.get():
-            if event.type == QUIT:
+            if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
                 pygame.quit()
                 sys.exit()
 
             if event.type == KEYDOWN and event.key == K_SPACE:
                 graph = rand_graph()
-        
-        # Render
-        displaysurface.fill((51,51,51))
-        
-        for edge in graph.edges:
-            draw.line(displaysurface, (255, 255, 255), graph.nodes[edge[0]]['position'], graph.nodes[edge[1]]['position'], 3 )
+                player_influence_radius = player_influence_radius * 1.25
 
+        # Render
+        transparency_surface.fill((51,51,51,0))
+        displaysurface.fill((51,51,51))
+
+        for edge in graph.edges:
+            draw.aaline(displaysurface, (255, 255, 255), graph.nodes[edge[0]]['position'], graph.nodes[edge[1]]['position'], 1 )
+
+        # Sphere of Influence Generation
+        for _, node_data in graph.nodes(data=True):
+            draw.circle( transparency_surface, color=(node_data['color'][0], node_data['color'][1], node_data['color'][2], 50), center=node_data['position'], radius=player_influence_radius)
+        
         for _, node_data in graph.nodes(data=True):
             draw.circle( displaysurface, color=node_data['color'], center=node_data['position'], radius=NODE_RADIUS)
         
+        # Draws the second transparency_surface overtop the displaysurface.
+        displaysurface.blit(transparency_surface, (0,0))
+
         pygame.display.update()
         FramePerSec.tick(FPS)
 
